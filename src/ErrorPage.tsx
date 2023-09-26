53032145ffecbddcb2ef0d9846eb23f6c0836dbc
Preslav <preslavk1201@gmail.com>
2023-09-26 15:14:03 +0300

    temp fix

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
     const error: any = useRouteError();
@@ -11,6 +11,7 @@ export default function ErrorPage() {
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
+        <Link to={`BRS-4/login`}>Login</Link>
       </div>
     );
   }
