31c9682123ca95abd62bfe0e375aee43ad64d423
Preslav <preslavk1201@gmail.com>
2023-09-30 16:15:07 +0300

    navigation design

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,20 +1,42 @@
+import { Logout, Menu } from "@mui/icons-material";
+import { IconButton, List, ListItemButton, ModalClose } from "@mui/joy";
+import Drawer from "@mui/joy/Drawer/Drawer";
+import { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 export function Layout() {
-    //redirect if not logged
-    return (
-        <div className="flex">
-            <div className="h-screen w-[140px] bg-slate-700 flex flex-col justify-between text-white gap-4">
-                <div className="flex flex-col items-center">
-                    <NavLink to={`clients`} className='w-full py-4 text-center hover:bg-slate-800 transition-all duration-[350ms]'>Clients</NavLink>
-                    <NavLink to={`policies`} className='w-full py-4 text-center hover:bg-slate-800 transition-all duration-[350ms]'>Policies</NavLink>
-                </div>
-                <div className="flex flex-col items-center">
-                    <NavLink to={`login`} className='w-full py-4 text-center hover:bg-slate-800 transition-all duration-[350ms]'>Logout</NavLink>
-                </div>
-            </div>
 
+    const [drawerOpen, setDrawerOpen] = useState(false)
+
+    return (
+        <>
+            <IconButton variant="outlined" color="primary" onClick={() => setDrawerOpen(true)} sx={{ margin: '10px' }}>
+                <Menu />
+            </IconButton>
+            <Drawer open={drawerOpen} size="sm" onClose={() => setDrawerOpen(false)} sx={{ display: 'flex', alignItems: 'center' }}>
+                <div className="flex justify-center h-auto py-4">
+                    <ModalClose id="close-icon" size="lg" sx={{ right: 'inherit' }} />
+                </div>
+                <div className="flex flex-col justify-between h-[80vh] flex-auto">
+                    <List component="nav" onClick={() => setDrawerOpen(false)} sx={{ paddingTop: '5vh' }} size="lg">
+                        <NavLink to={`clients`}>
+                            <ListItemButton sx={{ display: 'flex', justifyContent: 'center' }}>
+                                Клиенти
+                            </ListItemButton>
+                        </NavLink>
+                        <NavLink to={`policies`}>
+                            <ListItemButton sx={{ display: 'flex', justifyContent: 'center' }}>
+                                Полици
+                            </ListItemButton>
+                        </NavLink>
+                    </List>
+                    <IconButton variant="plain" color="danger" onClick={() => setDrawerOpen(true)} sx={{ margin: '10px' }}>
+                        <NavLink to={`login`} className={'mr-4'}>Излизане</NavLink>
+                        <Logout />
+                    </IconButton>
+                </div>
+            </Drawer>
             <Outlet />
-        </div>
+        </>
     )
 }
