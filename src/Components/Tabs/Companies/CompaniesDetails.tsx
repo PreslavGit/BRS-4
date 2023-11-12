f63b359b79ea7a027f01d0cfaa915a70f9802d62
RadiPopova <raditto22@gmail.com>
2023-11-12 14:51:08 +0200

    company details get data from db

--- src/Components/Tabs/Companies/CompaniesDetails.tsx
+++ src/Components/Tabs/Companies/CompaniesDetails.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@mui/joy";
 import { Company } from "./Companies"
 import { AccountBalance, Add, AlternateEmail, Phone } from "@mui/icons-material";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getCompanyById } from "../../../APIService";
 
 
 export function CompaniesDetails() {
@@ -11,6 +13,17 @@ export function CompaniesDetails() {
     c.INS_COMPANY_BULSTAT = "981273238"
     c.INS_COMPANY_CONTACT = "office@armeec.bg"
     c.INS_COMPANY_TEL = "+359 (0)700 1 3939"
+    const [comp, setComp] = useState(c)
+    const params = useParams()
+
+    useEffect(() => {
+        getCompanyById(Number(params.id))
+            .then(res => { 
+                if (res && res.length){ 
+                    setComp(res[0]) 
+                } 
+            })
+    }, [])
 
     const products = [
         { name: 'Продукт 1', obj: 'Живот'},
@@ -25,21 +38,21 @@ export function CompaniesDetails() {
         <>
          <div className="sm:w-[1000px] h-[500px] p-4 rounded-xl border-blue-300 border-[1px] m-auto mt-12 bg-gradient-to-tl from-sky-50 to-sky-100 shadow-xl">
                 <div className="text-center">
-                    <h1 className="text-4xl font-bold text-sky-900 pt-4">{c.INS_COMPANY_NAME}</h1>
-                    <div className="text-stone-600 font-light text-sm">{c.INS_COMPANY_ADDR}</div>
+                    <h1 className="text-4xl font-bold text-sky-900 pt-4">{comp.INS_COMPANY_NAME}</h1>
+                    <div className="text-stone-600 font-light text-sm">{comp.INS_COMPANY_ADDR}</div>
                 </div>
                 <div className="flex gap-8 mt-6 w-[80%] m-auto justify-center">
                     <div className="flex gap-2">
                         <Phone className="text-sky-900"/>
-                        <a href={"tel: " + c.INS_COMPANY_TEL} className="underline">{c.INS_COMPANY_TEL}</a>
+                        <a href={"tel: " + comp.INS_COMPANY_TEL} className="underline">{comp.INS_COMPANY_TEL}</a>
                     </div>
                     <div className="flex gap-2">
                         <AlternateEmail className="text-sky-900"/>
-                        <a href={"mailto: " + c.INS_COMPANY_CONTACT} className="underline">{c.INS_COMPANY_CONTACT}</a>
+                        <a href={"mailto: " + comp.INS_COMPANY_CONTACT} className="underline">{comp.INS_COMPANY_CONTACT}</a>
                     </div>
                     <div className="flex gap-2">
                         <AccountBalance className="text-sky-900"/>
-                        <div>{c.INS_COMPANY_BULSTAT}</div>
+                        <div>{comp.INS_COMPANY_BULSTAT}</div>
                     </div>
                 </div>
                 <div className="flex flex-col items-center">
@@ -50,12 +63,14 @@ export function CompaniesDetails() {
                     <div>
                         <table>
                             <thead>
-                                <th className="bg-sky-900 text-white p-2 rounded-tl-md text-left">Име</th>
-                                <th className="bg-sky-900 text-white p-2 rounded-tr-md text-left">Обект</th>
+                                <tr>
+                                    <th className="bg-sky-900 text-white p-2 rounded-tl-md text-left">Име</th>
+                                    <th className="bg-sky-900 text-white p-2 rounded-tr-md text-left">Обект</th>
+                                </tr>
                             </thead>
                             <tbody>
                                 {products.map((p, i) => {
-                                    return <tr 
+                                    return <tr key={i}
                                         className={"transition-all duration-200 hover:translate-x-1 " + (i % 2 == 0 ? 'bg-slate-300' : 'bg-slate-200')}>
                                         <td className="w-[160px] px-2 p-1">{p.name}</td>
                                         <td className="w-[160px] px-2 p-1">{p.obj}</td>
